64d263c7486377d09a93014b145b820279fc5a0d
romedu <32285662+romedu@users.noreply.github.com>
2019-02-13 22:22:00 -0400

    Update UI

--- client/src/components/LandingPage/InfoSection/InfoSection.js
+++ client/src/components/LandingPage/InfoSection/InfoSection.js
@@ -1,21 +1,21 @@
 import React from "react";
 import InfoBlock from "../InfoBlock/InfoBlock";
-import resortImage from "../../../assets/images/luxury-resort.jpg";
-import nodeHerokuImage from "../../../assets/images/node-&-heroku.png";
-import reactImage from "../../../assets/images/reactJS-wallpaper.png";
+import UIImage from "../../../assets/images/user-interface.png";
+import sortingImage from "../../../assets/images/sorting.png";
+import foldersImage from "../../../assets/images/folders.png";
 import "./InfoSection.css";
 
 const InfoSection = props => {
    return (
       <section className="InfoSection">
-         <InfoBlock title="Lorem ipsum dolor" image={reactImage} backgroundColor="#CFBAE1">
-            Lorem ipsum dolor sit amet, nec nemore causae prompta ut. Vel idque persequeris id, vel cu volumus intellegam deterruisset. 
+         <InfoBlock title="Organize your todos in folders" image={foldersImage} backgroundColor="#CFBAE1">
+            With our folder system, you can easily create todos and group them according to your own criteria.
          </InfoBlock>
-         <InfoBlock title="Lorem ipsum dolor" image={nodeHerokuImage} backgroundColor="#FEC3A6">
-            Lorem ipsum dolor sit amet, nec nemore causae prompta ut. Vel idque persequeris id, vel cu volumus intellegam deterruisset.
+         <InfoBlock title="Smooth User Interface" image={UIImage} backgroundColor="#fcb38f">
+            The user interface was designed thinking in the casual user, who doesn't want to learn anything new in order to use it.
          </InfoBlock>
-         <InfoBlock title="Lorem ipsum dolor" image={resortImage} backgroundColor="#EFE9AE">
-            Lorem ipsum dolor sit amet, nec nemore causae prompta ut. Vel idque persequeris id, vel cu volumus intellegam deterruisset.
+         <InfoBlock title="Live Sorting" image={sortingImage} backgroundColor="#EFE9AE">
+            Our sorting algorithm supports multiple ways to organize your items, making sure you find the one suited for your needs.
          </InfoBlock>
       </section>
    )
