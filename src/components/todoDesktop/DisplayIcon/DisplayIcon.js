52f7a726069f6fd75400476330f7a243f0af0e00
romedu <romedu1@hotmail.com>
2020-09-15 14:07:35 -0400

    Refactor the DisplayIcon and IconList todoDesktop components

--- src/components/todoDesktop/DisplayIcon/DisplayIcon.js
+++ src/components/todoDesktop/DisplayIcon/DisplayIcon.js
@@ -1,45 +1,36 @@
-import React, {Fragment} from "react";
-import {Link} from "react-router-dom";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faTrashAlt, faPencilAlt} from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 import Button from "../../UI/Button/Button";
 import ButtonGroup from "../../UI/ButtonGroup/ButtonGroup";
 import "./DisplayIcon.css";
 
-const DisplayIcon = props => {
-   const {openHandler, settingsHandler, deleteHandler, ...iconData} = props,
-         content = (
-            <Fragment>
-               <div className={`${iconData.type}Icon`} title={iconData.description}>
-                  <img src={iconData.image || ""} alt="" />
-               </div>
-               {iconData.name}
-            </Fragment>
-         );
-
-   return (
-      <li className="displayIcon" onClick={openHandler}>
-         <ButtonGroup groupType="settingsGroup">
-            <Button design="settings" action={e => {
-                  e.stopPropagation(); 
-                  deleteHandler(iconData)
-            }}>
-               <FontAwesomeIcon size={window.screen.availWidth < 600 ? "2x" : "1x"} icon={faTrashAlt} color="rgb(91, 146, 130)" />
-            </Button>
-            <Button design="settings" action={e => {
-                  e.stopPropagation(); 
-                  settingsHandler(iconData)
-            }}>
-               <FontAwesomeIcon size={window.screen.availWidth < 600 ? "2x" : "1x"} icon={faPencilAlt} color="rgb(91, 146, 130)" />
-            </Button>
-         </ButtonGroup>
-         {iconData.url 
-          ? <Link to={iconData.url}>
-             {content} 
-          </Link>
-          : content}
-      </li>
-   )
-};
+const DisplayIcon = ({ name, type, image, description, itemId }) => (
+	<li className="displayIcon">
+		<ButtonGroup groupType="settingsGroup">
+			<Button design="settings">
+				<FontAwesomeIcon
+					size={window.screen.availWidth < 600 ? "2x" : "1x"}
+					icon={faTrashAlt}
+					color="rgb(91, 146, 130)"
+				/>
+			</Button>
+			<Button design="settings">
+				<FontAwesomeIcon
+					size={window.screen.availWidth < 600 ? "2x" : "1x"}
+					icon={faPencilAlt}
+					color="rgb(91, 146, 130)"
+				/>
+			</Button>
+		</ButtonGroup>
+		<Link to={`/desktop/${itemId}`}>
+			<div className={`${type}Icon`} title={description}>
+				<img src={image || ""} alt="" />
+			</div>
+			{name}
+		</Link>
+	</li>
+);
 
 export default DisplayIcon;
--- src/components/todoDesktop/IconList/IconList.js
+++ src/components/todoDesktop/IconList/IconList.js
@@ -2,28 +2,16 @@ import React from "react";
 import DisplayIcon from "../DisplayIcon/DisplayIcon";
 import "./IconList.css";
 
-const IconList = props => {
-   const {folders, todos, insideFolder, openHandler, settingsHandler, deleteHandler} = props,
-         folderList = folders && folders.map(folder => {
-            const {files, ...folderData} = folder;
-            return <DisplayIcon type="folder" key={folder._id} {...folderData} openHandler={() => openHandler(folder._id)} settingsHandler={settingsHandler} deleteHandler={deleteHandler} />
-         }),
-         todosList = todos && todos.map(todo => <DisplayIcon type="todo" key={todo._id} {...todo} url={`/todos/${todo._id}`} settingsHandler={settingsHandler} deleteHandler={deleteHandler} />);
-               
-   return (
-      <div className="IconList">
-         {!insideFolder && <h3>
-            {todosList && "Todolists"}
-            {folders && "Folders"}
-         </h3>} 
-         {todosList && <ul>
-            {todosList}
-         </ul>}
-         {folderList && <ul>
-            {folderList}
-         </ul>}
-      </div>
-   )
+const IconList = ({ items }) => {
+	const itemsList = items.map(item => (
+		<DisplayIcon type={item.files ? "folder" : "todo"} name={item.name} itemId={item._id} />
+	));
+
+	return (
+		<div className="IconList">
+			<ul> {itemsList} </ul>
+		</div>
+	);
 };
 
 export default IconList;
