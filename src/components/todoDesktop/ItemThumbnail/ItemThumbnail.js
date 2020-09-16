f652510bb5adffdda8d043d27d4f2631778b972b
romedu <romedu1@hotmail.com>
2020-09-16 13:10:47 -0400

    Redesign the ItemThumbnail component

--- src/components/todoDesktop/ItemThumbnail/ItemThumbnail.js
+++ src/components/todoDesktop/ItemThumbnail/ItemThumbnail.js
@@ -1,32 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashAlt, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
-import Button from "../../UI/Button/Button";
-import ButtonGroup from "../../UI/ButtonGroup/ButtonGroup";
 import "./ItemThumbnail.css";
 
 const ItemThumbnail = ({ name, type, image, description, itemId }) => (
 	<li className="itemThumbnail">
-		<ButtonGroup groupType="settingsGroup">
-			<Button design="settings">
-				<FontAwesomeIcon
-					size={window.screen.availWidth < 600 ? "2x" : "1x"}
-					icon={faTrashAlt}
-					color="rgb(91, 146, 130)"
-				/>
-			</Button>
-			<Button design="settings">
-				<FontAwesomeIcon
-					size={window.screen.availWidth < 600 ? "2x" : "1x"}
-					icon={faPencilAlt}
-					color="rgb(91, 146, 130)"
-				/>
-			</Button>
-		</ButtonGroup>
 		<Link to={`/desktop/${itemId}`}>
 			<div className={`${type}Icon`} title={description}>
-				<img src={image || ""} alt="" />
+				{image && <img src={image} alt="" />}
 			</div>
 			{name}
 		</Link>
