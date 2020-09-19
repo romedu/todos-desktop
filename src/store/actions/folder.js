695c9a058ce0c7749814bd81961438182897f79d
romedu <romedu1@hotmail.com>
2020-09-19 18:12:11 -0400

    Refactor the folder actions

--- src/store/actions/folder.js
+++ src/store/actions/folder.js
@@ -1,22 +1,6 @@
-import qwest from "qwest";
+import { makeDeleteRequest, makeGetRequest, makePatchRequest, makePostRequest } from "../../utilities/services";
 import actionTypes from "./actionTypes";
 import { createMessage } from "./message";
-qwest.limit(2);
-
-export const getFolders = (sortProp, sortOrder, pageParam) => {
-	const headers = { Authorization: localStorage.getItem("token") };
-	return dispatch => {
-		qwest
-			.get(`/api/folder?sortProp=${sortProp}&sortOrder=${sortOrder}&page=${pageParam}&limit=14`, null, { headers })
-			.then(data => JSON.parse(data.response))
-			.then(response => {
-				const { status, message, docs, total, limit } = response;
-				if (status && status !== 200) throw new Error(message);
-				return dispatch(setFolders(docs, { limit, total }));
-			})
-			.catch(error => dispatch(createMessage("Error", error.message)));
-	};
-};
 
 const setFolders = (folders, paginationData) => ({
 	type: actionTypes.GET_FOLDERS,
@@ -24,90 +8,95 @@ const setFolders = (folders, paginationData) => ({
 	paginationData
 });
 
-export const openFolder = folderId => {
-	const headers = { Authorization: localStorage.getItem("token") };
-	return dispatch => {
-		qwest
-			.get(`/api/folder/${folderId}`, null, { headers })
-			.then(data => JSON.parse(data.response))
-			.then(response => {
-				const { status, message, ...folder } = response;
-				if (status && status !== 200) throw new Error(message);
-				return dispatch(setCurrent(folder));
-			})
-			.catch(error => dispatch(createMessage("Error", error.message)));
-	};
-};
-
 const setCurrent = folder => ({
 	type: actionTypes.OPEN_FOLDER,
 	folder
 });
-
-export const closeFolder = () => ({
-	type: actionTypes.CLOSE_FOLDER
-});
-
-export const createFolder = newFolderData => {
-	const headers = { Authorization: localStorage.getItem("token") };
-	return dispatch => {
-		qwest
-			.post("/api/folder", newFolderData, { headers })
-			.then(data => JSON.parse(data.response))
-			.then(response => {
-				const { status, message, ...newFolder } = response;
-				if (status && status !== 201) throw new Error(message);
-				return dispatch(addFolder(newFolder));
-			})
-			.catch(error => dispatch(createMessage("Error", error.message)));
-	};
-};
 
 const addFolder = newFolder => ({
 	type: actionTypes.CREATE_FOLDER,
 	newFolder
 });
 
-export const updateFolder = (folderId, payload) => {
-	const headers = { Authorization: localStorage.getItem("token") };
-	return dispatch => {
-		qwest
-			.map("PATCH", `/api/folder/${folderId}`, payload, { headers })
-			.then(data => JSON.parse(data.response))
-			.then(response => {
-				const { status, message, ...editedFolder } = response;
-				if (status && status !== 200) throw new Error(message);
-				return dispatch(editFolder(editedFolder));
-			})
-			.catch(error => dispatch(createMessage("Error", error.message)));
-	};
-};
-
 const editFolder = editedFolder => ({
 	type: actionTypes.UPDATE_FOLDER,
 	editedFolder
 });
 
-export const deleteFolder = (folderId, keep) => {
-	const headers = { Authorization: localStorage.getItem("token") },
-		keepParam = keep ? "&keep=true" : "";
-
-	return dispatch => {
-		qwest["delete"](`/api/folder/${folderId}?${keepParam}`, null, { headers })
-			.then(data => JSON.parse(data.response))
-			.then(response => {
-				const { status, message } = response;
-				if (status && status !== 200) throw new Error(message);
-				return dispatch(removeFolder(folderId));
-			})
-			.catch(error => dispatch(createMessage("Error", error.message)));
-	};
-};
-
 const removeFolder = folderId => ({
 	type: actionTypes.DELETE_FOLDER,
 	folderId
 });
+
+export const getFolders = (sortProp, sortOrder, pageParam) => {
+	return async dispatch => {
+		try {
+			const reqUrl = `/api/folder?sortProp=${sortProp}&sortOrder=${sortOrder}&page=${pageParam}&limit=14`;
+			const { docs, total, limit } = await makeGetRequest(reqUrl);
+
+			return dispatch(setFolders(docs, { limit, total }));
+		} catch (error) {
+			dispatch(createMessage("Error", error.message));
+		}
+	};
+};
+
+export const openFolder = folderId => {
+	return async dispatch => {
+		try {
+			const reqUrl = `/api/folder/${folderId}`;
+			const folder = await makeGetRequest(reqUrl);
+
+			return dispatch(setCurrent(folder));
+		} catch (error) {
+			dispatch(createMessage("Error", error.message));
+		}
+	};
+};
+
+export const closeFolder = () => ({
+	type: actionTypes.CLOSE_FOLDER
+});
+
+export const createFolder = newFolderData => {
+	return async dispatch => {
+		try {
+			const reqUrl = `/api/folder`;
+			const newFolder = await makePostRequest(reqUrl, newFolderData);
+
+			return dispatch(addFolder(newFolder));
+		} catch (error) {
+			dispatch(createMessage("Error", error.message));
+		}
+	};
+};
+
+export const updateFolder = (folderId, payload) => {
+	return async dispatch => {
+		try {
+			const reqUrl = `/api/folder/${folderId}`;
+			const editedFolder = await makePatchRequest(reqUrl, payload);
+
+			return dispatch(editFolder(editedFolder));
+		} catch (error) {
+			dispatch(createMessage("Error", error.message));
+		}
+	};
+};
+
+export const deleteFolder = (folderId, shouldKeepTodoLists) => {
+	return async dispatch => {
+		try {
+			const keepParam = shouldKeepTodoLists ? "&keep=true" : "";
+			const reqUrl = `/api/folder/${folderId}?${keepParam}`;
+
+			await makeDeleteRequest(reqUrl);
+			return dispatch(removeFolder(folderId));
+		} catch (error) {
+			dispatch(createMessage("Error", error.message));
+		}
+	};
+};
 
 export const addFile = newFile => ({
 	type: actionTypes.ADD_FILE,
