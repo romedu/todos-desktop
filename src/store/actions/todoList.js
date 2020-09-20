ea216d79b7996c840d8e869b183504c00460ebe3
romedu <romedu1@hotmail.com>
2020-09-20 16:19:53 -0400

    Refactor the todoList actions

--- src/store/actions/todoList.js
+++ src/store/actions/todoList.js
@@ -1,25 +1,7 @@
-import qwest from "qwest";
 import actionTypes from "./actionTypes";
 import { createMessage } from "./message";
 import { addFile, removeFile } from "./folder";
-qwest.limit(2);
-
-export const getLists = (sortProp, sortOrder, pageNum) => {
-	const headers = { Authorization: localStorage.getItem("token") };
-	return dispatch => {
-		qwest
-			.get(`/api/todoList?sortProp=${sortProp}&sortOrder=${sortOrder}&folderLess=true&page=${pageNum}&limit=14`, null, {
-				headers
-			})
-			.then(data => JSON.parse(data.response))
-			.then(response => {
-				const { status, message, docs, total, limit } = response;
-				if (status && status !== 200) throw new Error(message);
-				return dispatch(setLists(docs, { limit, total }));
-			})
-			.catch(error => dispatch(createMessage("Error", error.message)));
-	};
-};
+import { makeDeleteRequest, makeGetRequest, makePatchRequest, makePostRequest } from "../../utilities/services";
 
 const setLists = (lists, paginationData) => ({
 	type: actionTypes.GET_LISTS,
@@ -27,98 +9,99 @@ const setLists = (lists, paginationData) => ({
 	paginationData
 });
 
-export const openList = listId => {
-	const headers = { Authorization: localStorage.getItem("token") };
-	return dispatch => {
-		qwest
-			.get(`/api/todoList/${listId}`, null, { headers })
-			.then(data => JSON.parse(data.response))
-			.then(response => {
-				const { status, message, ...list } = response;
-				if (status && status !== 200) throw new Error(message);
-				return dispatch(setCurrent(list));
-			})
-			.catch(error => dispatch(createMessage("Error", error.message)));
-	};
-};
-
 const setCurrent = list => ({
 	type: actionTypes.OPEN_LIST,
 	list
 });
-
-export const closeList = () => ({
-	type: actionTypes.CLOSE_LIST
-});
-
-export const createList = (newListData, insideFolder) => {
-	const headers = { Authorization: localStorage.getItem("token") };
-	return dispatch => {
-		qwest
-			.post("/api/todoList", newListData, { headers })
-			.then(data => JSON.parse(data.response))
-			.then(response => {
-				const { status, message, ...newList } = response;
-				if (status && status !== 201) throw new Error(message);
-				if (newList.folderName) {
-					if (insideFolder) return dispatch(addFile(newList));
-					return dispatch(createMessage("Notification", `New list was added to the "${newList.folderName}" Folder`));
-				}
-				return dispatch(addList(newList));
-			})
-			.catch(error => dispatch(createMessage("Error", error.message)));
-	};
-};
 
 const addList = newList => ({
 	type: actionTypes.CREATE_LIST,
 	newList
 });
 
-export const moveList = (listId, moveFrom, moveTo) => {
-	const payload = { container: moveTo };
-	const headers = { Authorization: localStorage.getItem("token") };
-
-	return dispatch => {
-		qwest
-			.map("PATCH", `/api/todoList/${listId}`, payload, { headers })
-			.then(data => JSON.parse(data.response))
-			.then(response => {
-				const { status, message, ...movedList } = response;
-				const isListInDesktop = !moveFrom;
-
-				if (status && status !== 200) throw new Error(message);
-				if (isListInDesktop) dispatch(removeList(movedList._id));
-				else {
-					dispatch(removeFile(movedList._id));
-					dispatch(addList(movedList));
-				}
-			})
-			.catch(error => dispatch(createMessage("Error", error.message)));
-	};
-};
-
 const editList = editedList => ({
 	type: actionTypes.UPDATE_LIST,
 	editedList
 });
 
-export const deleteList = (listId, insideFolder) => {
-	const headers = { Authorization: localStorage.getItem("token") };
-	return dispatch => {
-		qwest["delete"](`/api/todoList/${listId}`, null, { headers })
-			.then(data => JSON.parse(data.response))
-			.then(response => {
-				const { status, message } = response;
-				if (status && status !== 200) throw new Error(message);
-				if (insideFolder) return dispatch(removeFile(listId));
-				return dispatch(removeList(listId));
-			})
-			.catch(error => dispatch(createMessage("Error", error.message)));
-	};
-};
-
 const removeList = listId => ({
 	type: actionTypes.DELETE_LIST,
 	listId
 });
+
+export const getLists = (sortProp, sortOrder, pageNum) => {
+	return async dispatch => {
+		try {
+			const reqUrl = `/api/todoList?sortProp=${sortProp}&sortOrder=${sortOrder}&folderLess=true&page=${pageNum}&limit=14`;
+			const { docs, total, limit } = await makeGetRequest(reqUrl);
+
+			return dispatch(setLists(docs, { limit, total }));
+		} catch (error) {
+			dispatch(createMessage("Error", error.message));
+		}
+	};
+};
+
+export const openList = listId => {
+	return async dispatch => {
+		try {
+			const reqUrl = `/api/todoList/${listId}`;
+			const todoList = await makeGetRequest(reqUrl);
+
+			return dispatch(setCurrent(todoList));
+		} catch (error) {
+			dispatch(createMessage("Error", error.message));
+		}
+	};
+};
+
+export const closeList = () => ({
+	type: actionTypes.CLOSE_LIST
+});
+
+export const createList = newListData => {
+	return async dispatch => {
+		try {
+			const reqUrl = "/api/todoList";
+			const newList = await makePostRequest(reqUrl, newListData);
+
+			if (newList.container) return dispatch(addFile(newList));
+			else return dispatch(addList(newList));
+		} catch (error) {
+			dispatch(createMessage("Error", error.message));
+		}
+	};
+};
+
+export const moveList = (listId, moveFrom, moveTo) => {
+	return async dispatch => {
+		try {
+			const reqUrl = `/api/todoList/${listId}`;
+			const payload = { container: moveTo };
+			const movedList = await makePatchRequest(reqUrl, payload);
+			const isListInDesktop = !moveFrom;
+
+			if (isListInDesktop) dispatch(removeList(movedList._id));
+			else {
+				dispatch(removeFile(movedList._id));
+				dispatch(addList(movedList));
+			}
+		} catch (error) {
+			dispatch(createMessage("Error", error.message));
+		}
+	};
+};
+
+export const deleteList = (listId, insideFolder) => {
+	return async dispatch => {
+		try {
+			const reqUrl = `/api/todoList/${listId}`;
+
+			await makeDeleteRequest(reqUrl);
+			if (insideFolder) return dispatch(removeFile(listId));
+			return dispatch(removeList(listId));
+		} catch (error) {
+			dispatch(createMessage("Error", error.message));
+		}
+	};
+};
