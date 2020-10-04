7ade7e30d9a4ff0a581fb1f507a7d0b5650510e2
roberto-epayco <romedu1@hotmail.com>
2020-10-04 19:27:19 -0400

    Create the updateProp data utility function

--- src/utilities/data.js
+++ src/utilities/data.js
@@ -1,5 +1,7 @@
 export const findByProp = (prop, propValue, arr) => arr.find(item => item[prop] === propValue);
 
+export const updateProp = (propName, propValue, obj) => ({ ...obj, [propName]: propValue });
+
 export const removeById = (id, arr) => arr.filter(item => item._id !== id);
 
 export const updateItem = (editedItem, arr) => arr.map(item => (item._id === editedItem._id ? editedItem : item));
