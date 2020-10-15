d5e939753e18dfcc1795d00f0a38a9958b89b7b7
roberto-epayco <romedu1@hotmail.com>
2020-10-15 00:29:02 -0400

    Manage the CreateFolderBtn conditional rendering process

--- src/components/todoDesktop/CreateItemBtns/CreateItemBtns.js
+++ src/components/todoDesktop/CreateItemBtns/CreateItemBtns.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { withRouter } from "react-router";
 import CreateFolderBtn from "../CreateFolderBtn";
 import CreateTodoListBtn from "../CreateTodoListBtn";
 import "./CreateItemBtns.css";
 
 const CreateItemBtns = props => {
+   const shouldShowFolderBtn = props.match.isExact;
+   
 	return (
 		<div className="createItemBtns">
 			<CreateTodoListBtn />
-			<CreateFolderBtn />
+			{shouldShowFolderBtn && <CreateFolderBtn />}
 		</div>
 	);
 };
 
-export default CreateItemBtns;
+export default withRouter(CreateItemBtns);
