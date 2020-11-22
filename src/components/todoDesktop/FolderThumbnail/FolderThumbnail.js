dc32fd4d7b077dedd9f54f221a261fcbf8a343f4
roberto-epayco <romedu1@hotmail.com>
2020-11-22 17:38:20 -0400

    Add the ItemForm and PageBlocker components to the FolderThumbnail

--- src/components/todoDesktop/FolderThumbnail/FolderThumbnail.js
+++ src/components/todoDesktop/FolderThumbnail/FolderThumbnail.js
@@ -1,42 +1,49 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { DropTarget } from "react-dnd";
-import { TODOLIST_DRAG_TYPE } from "../../../constants";
 import { Link, withRouter } from "react-router-dom";
+import ItemForm from "../ItemForm/ItemForm";
+import PageBlocker from "../../UI/PageBlocker/PageBlocker";
+import { TODOLIST_DRAG_TYPE } from "../../../constants";
 import "./FolderThumnail.css";
 
 class FolderThumbnail extends Component {
-	state = {
-		openFolderTimeoutID: null
-   };
-   
-   componentDidUpdate(){
-      const {openFolderTimeoutID} = this.state;
-      const { isOver, canDrop, history, itemId } = this.props;
-      const folderPath = `/desktop/${itemId}`;
+	state = { openFolderTimeoutID: null };
 
-      if (isOver && !openFolderTimeoutID) this.setState({ openFolderTimeoutID: setTimeout(() => history.push(folderPath), 600) });
+	componentDidMount() {
+		this.setState(prevState => ({ ...prevState }));
+	}
+
+	componentDidUpdate() {
+		const { openFolderTimeoutID } = this.state;
+		const { isOver, canDrop, history, itemId } = this.props;
+		const folderPath = `/desktop/${itemId}`;
+
+		if (isOver && !openFolderTimeoutID) {
+			this.setState({ openFolderTimeoutID: setTimeout(() => history.push(folderPath), 600) });
+		}
 		if (openFolderTimeoutID && (!isOver || !canDrop)) {
 			clearTimeout(openFolderTimeoutID);
 			this.setState({ openFolderTimeoutID: null });
 		}
-   }
+	}
 
 	render() {
 		const { connectDropTarget, isOver, name, description, itemId } = this.props;
 		const folderPath = `/desktop/${itemId}`;
-		let thumbnailClassName = "folderThumbnail";
-
-      if(isOver) thumbnailClassName = thumbnailClassName.concat(" ", "overFolderThumbnail");
+		const thumbnailClassName = isOver ? "folderThumbnail overFolderThumbnail" : "folderThumbnail";
 
 		return (
-			<div ref={connectDropTarget}>
-				<Link to={folderPath}>
-					<li className="itemThumbnail">
-						<div className={thumbnailClassName} title={description}></div>
-						{name}
-					</li>
-				</Link>
-			</div>
+			<Fragment>
+				{itemId === "untitled" && <PageBlocker />}
+				<div ref={connectDropTarget}>
+					<Link to={folderPath}>
+						<li className="itemThumbnail">
+							<div className={thumbnailClassName} title={description}></div>
+							{itemId === "untitled" ? <ItemForm createHandler={newFolderName => {}} /> : name}
+						</li>
+					</Link>
+				</div>
+			</Fragment>
 		);
 	}
 }
