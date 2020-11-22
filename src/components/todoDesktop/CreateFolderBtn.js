f1d37064ab5feb4fad744c9163d8cb92b31f0d57
roberto-epayco <romedu1@hotmail.com>
2020-11-22 17:30:41 -0400

    Add the creation of untilted folders feature to the createFolderBtn component

--- src/components/todoDesktop/CreateFolderBtn.js
+++ src/components/todoDesktop/CreateFolderBtn.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createUntitledFolder } from "../../store/actions/folder";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFolderPlus } from "@fortawesome/free-solid-svg-icons";
 
-const CreateFolderBtn = props => {
+const CreateFolderBtn = ({ createNewFolder }) => {
 	return (
 		<button className="TransparentBtn">
-			<FontAwesomeIcon size="4x" icon={faFolderPlus} color="#7fd1b9" onClick={() => {}} />
+			<FontAwesomeIcon size="4x" icon={faFolderPlus} color="#7fd1b9" onClick={createNewFolder} />
 		</button>
 	);
 };
 
-export default CreateFolderBtn;
+const mapDispatchToProps = dispatch => ({
+	createNewFolder: () => dispatch(createUntitledFolder())
+});
+
+export default connect(null, mapDispatchToProps)(CreateFolderBtn);
